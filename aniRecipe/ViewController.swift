9f8599deeb4fd0b3e81d3e4e38a33d9c5ca8a6cd
Kaho Hayashi <kaho-hayashi@cookpad.com>
2019-08-27 14:15:03 +0900

    Initial Commit

---
+++ aniRecipe/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  aniRecipe
+//
+//  Created by kaho-hayashi on 2019/08/27.
+//  Copyright © 2019 Cookpad Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
